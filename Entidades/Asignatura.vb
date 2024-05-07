276c3faf4fa24eac4a926a14587da1a012c10ac0
Asier <asielng2a@gmail.com>
2024-05-07 13:10:40 +0200

    Creadas las entidades restantes(profesor,  Curso .......), cambio nombres de formularios

---
+++ Entidades/Asignatura.vb
@@ -0,0 +1,6 @@
+﻿Public Class Asignatura
+    Public Property NombreAsignatura As String
+    Public Sub New(nombreAsignatura As String)
+        Me.NombreAsignatura = nombreAsignatura
+    End Sub
+End Class
