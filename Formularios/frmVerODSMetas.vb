e9bed11f00061ab7bc9076d7a25edd35e66babf1
Ibai <ibai.loharces@gmail.com>
2024-05-09 13:07:42 +0200

    Hacer que en el datagridview de ver metas se pueda leer las descripciones

--- Formularios/frmVerODSMetas.vb
+++ Formularios/frmVerODSMetas.vb
@@ -9,9 +9,14 @@ Public Class frmVerODSMetas
         Dim odsSeleccionado As ODS = TryCast(cboODS.SelectedItem, ODS)
         dgvMetas.DataSource = gestion.VerMetasDeODS(odsSeleccionado.NumeroODS, mensajerror)
         pbODS.BackgroundImage = Image.FromFile($"imagenes/{odsSeleccionado.NumeroODS}.jpg")
+        For i = 0 To dgvMetas.ColumnCount - 1
+            dgvMetas.Columns.Item(i).Width = 300
+            dgvMetas.Rows.Item(i).Height = 100
+        Next
     End Sub
 
     Private Sub VerODSMetas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
         Dim msgError As String = ""
         Try
             cboODS.Items.AddRange(gestion.ODSEnBaseDeDatos(msgError).ToArray)
